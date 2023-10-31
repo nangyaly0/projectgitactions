90eea8b17bc9a5d28b636c84884f81d1ebe1cf68
ajmerriar <117775662+ajmerriar@users.noreply.github.com>
2023-10-31 03:12:19 -0400

    Update script.py

--- web.app/script.py
+++ web.app/script.py
@@ -1,1 +1,35 @@
-print("hello world")
+from flask import Flask, render_template, request, jsonify
+from PIL import Image
+import numpy as np
+from keras.models import load_model
+
+app = Flask(__name__)
+
+
+classes = {
+    0: 'its a cat',
+    1: 'its a dog'
+}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    try:
+        file = request.files['file']
+        image = Image.open(file)
+        image = image.resize((128, 128))
+        image = np.expand_dims(image, axis=0)
+        image = np.array(image)
+        image = image / 255
+        pred = model.predict([image])[0]
+        pred_class = np.argmax(pred)
+        sign = classes[pred_class]
+        return jsonify({'result': sign})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
